c11a83e42ff062fbf4ae7a6a3a353bf5d1d05f09
Seif (MAcbook M3) <vateseif@gmail.com>
2024-04-11 16:36:34 +0200

    fix teleoperation script

--- teleoperation.py
+++ teleoperation.py
@@ -1,12 +1,11 @@
 from robot import Robot
-from dynamixel import Dynamixel
+from config.config import ROBOT_PORTS
 
-leader_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='/dev/tty.usbmodem57380045631').instantiate()
-follower_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='/dev/tty.usbserial-FT8ISNO8').instantiate()
-follower = Robot(follower_dynamixel, servo_ids=[1, 2, 3, 4, 6, 7])
-leader = Robot(leader_dynamixel, servo_ids=[1, 2, 3, 4, 6, 7])
+# init robots
+leader = Robot(device_name=ROBOT_PORTS['leader'])
+follower = Robot(device_name=ROBOT_PORTS['follower'])
+# activate the leader gripper torque
 leader.set_trigger_torque()
-
 
 while True:
     follower.set_goal_pos(leader.read_position())
